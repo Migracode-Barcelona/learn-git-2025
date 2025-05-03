ad6d990369ba082a2f94a7b76f42c3b350fae9cb
Syed Arslan <32303617+SyedArslanHaider@users.noreply.github.com>
2025-05-03 12:23:09 +0200

    Add style to Greeting component

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,6 @@
+import './Greeting.css';
 function Greeting({ name }) {
-  return <h1>Hello my name is, {name}!</h1>;
+  return <h1 className="greeting-heading">Hello my name is, {name}!</h1>;
 }
 
 export default Greeting;
