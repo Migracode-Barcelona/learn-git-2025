acf7f9251f3dce310914f2e82680f7031aa1b1cf
Syed Arslan <32303617+SyedArslanHaider@users.noreply.github.com>
2025-05-03 11:59:35 +0200

    Add Greeting component to display a personalized welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from './Greeting';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Greeting name="Arslan Haider"/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
