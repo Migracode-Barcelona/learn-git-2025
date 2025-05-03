ac5f99e77b967d8f20aa7153d81010c456b37f96
Racheal Ehiwe <occ-l0030@OCC-L0030s-MacBook-Pro.local>
2025-05-03 12:01:20 +0200

    add new from page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import "./Form.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Form = () => {
+  return (
+    <div className="form-container">
+      <h2>Registration Form</h2>
+      <form>
+        <label>First Name:</label>
+        <input type="text" name="name" required />
+
+        <label>Surname:</label>
+        <input type="text" name="surname" required />
+
+        <label>Age:</label>
+        <input type="number" name="age" required />
+
+        <label>Gender:</label>
+        <select name="gender" required>
+          <option value="">--Select--</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+
+        <label>Email:</label>
+        <input type="email" name="email" />
+
+        <label>Message:</label>
+        <textarea name="message" rows="4"></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
